3ce97c1b902ad5c4b01e605d0b4b7ee7d49c97d0
Sanja Vadic <sanja.vadic@gmail.com>
2020-01-24 18:17:59 +0100

    Initial commit

---
+++ src/main/java/com/testdata/testdatabackend/TestDataBackendApplication.java
@@ -0,0 +1,13 @@
+package com.testdata.testdatabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDataBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testdata/testdatabackend/TestDataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testdata.testdatabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDataBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
